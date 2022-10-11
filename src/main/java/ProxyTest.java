f05d2bdb6cf983363b1f8434777049f3685315d9
BohdanBokii <bohdanbwork95@gmail.com>
2022-10-11 22:26:27 +0300

    -- Thread.sleep

--- src/main/java/ProxyTest.java
+++ src/main/java/ProxyTest.java
@@ -2,6 +2,7 @@ import DriverConfig.BaseClass;
 import Models.AGENTS;
 import Models.UserAgents;
 import Pages.YouTubeMain;
+import gherkin.lexer.Th;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
@@ -17,6 +18,7 @@ public class ProxyTest extends BaseClass {
     @Test
     public void test1() throws InterruptedException {
         youtube.goHome();
+        Thread.sleep(3000);
         youtube.search();
         Thread.sleep(3000);
         Util.ScreenShot.takeScreenShot(driver, "Screen1");
